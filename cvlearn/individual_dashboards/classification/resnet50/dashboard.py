7bbea18539b9975d57c91e2de9e3566ffda51acf
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-01 19:28:31 -0500

    Update

--- cvlearn/individual_dashboards/classification/resnet50/dashboard.py
+++ cvlearn/individual_dashboards/classification/resnet50/dashboard.py
@@ -11,20 +11,24 @@ def graph(prediction_classes,prediction_probabilities):
 
     fig,ax = plt.subplots()
 
-    ax.barh(0,prediction_probabilities[0])
-    ax.barh(1,prediction_probabilities[1])
-    ax.barh(2,prediction_probabilities[2])
+    ax.barh(0,prediction_probabilities[0]) #Adds the first bar
+    ax.barh(1,prediction_probabilities[1]) #Adds the second bar
+    ax.barh(2,prediction_probabilities[2]) #Adds the third bar
 
-    y = np.arange(3)
-    ax.set_yticks(y)
-    ax.set_yticklabels(prediction_classes)
+    y = np.arange(3) #Creates an array of [1,2,3] for the ticks
+    ax.set_yticks(y) #Creates ticks on graph 
+    ax.set_yticklabels(prediction_classes) #adds the labels
 
-    ax.set_xlabel('Probability')
+    #Adds titles and labels
+    ax.set_xlabel('Probability') 
     ax.set_title('Predictions')
 
     return fig
 
+
+@st.cache #Caches the model saves time when refreshing page
 def get_model():
+
     model = ResNet50(weights='imagenet')
     return model
 
@@ -32,22 +36,25 @@ if __name__ == "__main__":
 
     model = get_model()
 
-    img_path = '../images/car.jpg' #path for hte image
+    img_path = '../images/car.jpg' #path for the image
     img = image.load_img(img_path, target_size=(224, 224)) #loads the image and sizes accordingly
     x = image.img_to_array(img) #turns the image to an array so the model can use
-    x = np.expand_dims(x, axis=0) #Expands by adding a row.
+    x = np.expand_dims(x, axis=0) #Expands by a row to represent a batch size of 1. 
+    #preprocess_input needs aNumpy array encoding a batch of predictions.
     
-    x = preprocess_input(x) #Formats image for the way that model requires  
+    x = preprocess_input(x) #each Keras Application expects a specific kind of input preprocessing.
+    #For Xception, call tf.keras.applications.xception.preprocess_input on your inputs before passing them to the model.
 
     preds = model.predict(x) 
 
     # decode the results into a list of tuples (class, description, probability)
     # (one such list for each sample in the batch)
 
-    predictions = decode_predictions(preds, top=3)[0] 
-
+    predictions = decode_predictions(preds, top=3)[0] #only one sample in the batch [0]
+  
     prediction_classes = np.array([predictions[0][1],predictions[1][1],predictions[2][1]]) #Gets the top 3 classes
     prediction_probabilities = np.array([predictions[0][2],predictions[1][2],predictions[1][2]]) *100 #Gets the top 3 probablities
 
     fig = graph(prediction_classes,prediction_probabilities)
-    st.write(fig)
+
+    st.write(fig) #Writes the graph to streamlit
