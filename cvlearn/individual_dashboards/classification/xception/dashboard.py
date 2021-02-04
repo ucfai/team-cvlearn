a2c9ee5e136fc0053eab6ed011c008194c96d454
Kenley Rodriguez <kenleyrodriguez12@Kenleys-MacBook-Pro.local>
2021-02-04 12:58:23 -0500

    Update ken's dash

--- cvlearn/individual_dashboards/classification/xception/dashboard.py
+++ cvlearn/individual_dashboards/classification/xception/dashboard.py
@@ -1,1 +1,15 @@
-print("hello world")
+import streamlit as st
+import numpy as numpy
+from tensorflow import keras
+from keras.applications.xception import Xception
+from keras.applications.xception import preprocess_input, decode_predictions
+import matplotlib.pyplot as plt
+#import Image
+
+if __name__ == "__main__":
+
+    model = Xception(weights='imagenet')
+
+    picture = '/Users/kenleyrodriguez12/OneDrive - Knights - University of Central Florida/cvlearn/team-cvlearn/cvlearn/individual_dashboards/classification/images/car.jpg'
+    st.image(picture, caption='car', use_column_width=True)
+    x = "hello"
